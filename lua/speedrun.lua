853688a9d871019e2dc8051872cb56f37892e332
zanadoman <domanseven@gmail.com>
2024-05-07 22:04:04 +0200

    .

--- lua/speedrun.lua
+++ lua/speedrun.lua
@@ -4,11 +4,12 @@ function speedrun.setup(conf)
     vim.api.nvim_create_autocmd('BufEnter', {
         callback = function(opts)
             local lang = vim.bo[opts.buf].filetype
-            local key = conf.keymap or '<leader>r'
-            local icon = conf.langs[lang].icon or lang
 
             if conf.langs[lang]
             then
+                local key = conf.keymap or '<leader>r'
+                local icon = conf.langs[lang].icon or lang
+
                 for i, item in ipairs(conf.langs[lang].cmd)
                 do
                     vim.keymap.set('n', key .. i, ':terminal ' .. item or '' .. '\n', {
